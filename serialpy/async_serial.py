7853c81cc30af52709c79c274fa7c69227eedb3e
puddly <32534428+puddly@users.noreply.github.com>
2022-08-27 23:45:11 -0400

    Move async functions to `async_serial.py`

---
+++ serialpy/async_serial.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+import asyncio
+import logging
+import urllib.parse
+
+from serialpy.serial import ModemBits, Serial, STOPBITS_ONE, PARITY_NONE
+from serialpy.descriptor_transport import DescriptorTransport
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class SerialTransport(DescriptorTransport):
+    transport_name = "serial"
+
+    def __init__(
+        self,
+        loop,
+        protocol,
+        path,
+        baudrate,
+        stopbits=STOPBITS_ONE,
+        xonxoff=False,
+        rtscts=False,
+        waiter=None,
+        extra=None,
+    ):
+        super().__init__(loop, protocol, path, waiter, extra)
+        self._serial = Serial(
+            path=path,
+            baudrate=baudrate,
+            stopbits=stopbits,
+            xonxoff=xonxoff,
+            rtscts=rtscts,
+            # `DescriptorTransport` opened the port
+            fileno=self._fileno,
+        )
+        self._serial.configure_port()
+        self._extra["serial"] = self._serial
+
+    @property
+    def serial(self):
+        return self._serial
+
+
+async def create_serial_connection(
+    loop,
+    protocol_factory,
+    url,
+    baudrate,
+    parity=PARITY_NONE,
+    stopbits=STOPBITS_ONE,
+    xonxoff=False,
+    rtscts=False,
+    *,
+    transport_factory=SerialTransport,
+) -> tuple[SerialTransport, asyncio.Protocol]:
+    parsed_path = urllib.parse.urlparse(url)
+
+    if parsed_path.scheme in ("socket", "tcp"):
+        transport, protocol = await loop.create_connection(
+            lambda: protocol, parsed_path.hostname, parsed_path.port
+        )
+    else:
+        protocol = protocol_factory()
+        transport = transport_factory(
+            loop=loop,
+            protocol=protocol,
+            path=url,
+            baudrate=baudrate,
+            stopbits=stopbits,
+            xonxoff=xonxoff,
+            rtscts=rtscts,
+        )
+
+    return transport, protocol
+
+
+async def open_serial_connection(*args, **kwargs) -> tuple[asyncio.StreamReader, asyncio.StreamWriter]:
+    loop = asyncio.get_running_loop()
+
+    reader = asyncio.StreamReader(loop=loop)
+    protocol = asyncio.StreamReaderProtocol(reader, loop=loop)
+    transport, _ = await create_serial_connection(loop, lambda: protocol, *args, **kwargs)
+    writer = asyncio.StreamWriter(transport, protocol, reader, loop)
+
+    return reader, writer
