8ea03fbab22707687cc9c9f6b8eea6278e81a0df
puddly <32534428+puddly@users.noreply.github.com>
2022-09-20 12:53:09 -0400

    Implement a global `serialpy.patch()` function

--- serialpy/__init__.py
+++ serialpy/__init__.py
@@ -1,2 +1,12 @@
+import sys
+
 from serialpy.serial import ModemBits, Serial, STOPBITS_ONE, PARITY_NONE
 from serialpy.async_serial import SerialTransport, create_serial_connection, open_serial_connection
+
+
+def patch():
+    """
+    Patches `sys.modules` so that SerialPy replaces PySerial/PySerial-asyncio imports
+    """
+    sys.modules["serial"] = sys.modules[__name__]
+    sys.modules["serial_asyncio"] = sys.modules[__name__]
