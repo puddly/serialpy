cba5f9a42a35b1895760a48c8b2a5145f72ad748
puddly <32534428+puddly@users.noreply.github.com>
2024-01-03 16:47:54 -0500

    Patch `serial_asyncio_fast` as well

--- serialpy/__init__.py
+++ serialpy/__init__.py
@@ -3,10 +3,13 @@ import sys
 from serialpy.serial import ModemBits, Serial, STOPBITS_ONE, PARITY_NONE
 from serialpy.async_serial import SerialTransport, create_serial_connection, open_serial_connection
 
+_MODULES_TO_PATCH = ["serial", "serial_asyncio", "serial_asyncio_fast"]
+
 
 def patch():
     """
     Patches `sys.modules` so that SerialPy replaces PySerial/PySerial-asyncio imports
     """
-    sys.modules["serial"] = sys.modules[__name__]
-    sys.modules["serial_asyncio"] = sys.modules[__name__]
+
+    for module in _MODULES_TO_PATCH:
+        sys.modules[module] = sys.modules[__name__]
