b8ae60c1323713f7efe270b12318e4096c9602de
puddly <32534428+puddly@users.noreply.github.com>
2025-11-26 13:45:00 -0500

    Enable HUPCL on POSIX

--- serialx/common.py
+++ serialx/common.py
@@ -76,6 +76,7 @@ class BaseSerial(io.RawIOBase):
         *,
         buffer_character_count: int = 1,
         buffer_burst_timeout: float = 0.01,
+        hang_up_on_close: bool = True,
         exclusive: bool = True,
     ) -> None:
         """Initialize serial port configuration."""
@@ -94,6 +95,7 @@ class BaseSerial(io.RawIOBase):
         self._rtscts = rtscts
         self._parity = parity
         self._byte_size = byte_size
+        self._hang_up_on_close = hang_up_on_close
         self._exclusive = exclusive
 
         self._buffer_character_count = buffer_character_count
@@ -149,6 +151,11 @@ class BaseSerial(io.RawIOBase):
     def stopbits(self) -> StopBits:
         """Get the number of stop bits."""
         return self._stopbits
+
+    @property
+    def hang_up_on_close(self) -> bool:
+        """Get the hang up on close setting."""
+        return self._hang_up_on_close
 
     @property
     def exclusive(self) -> bool:
@@ -269,6 +276,12 @@ class BaseSerialTransport(asyncio.Transport):
         """Get the byte size."""
         assert self._serial is not None
         return self._serial.byte_size
+
+    @property
+    def hang_up_on_close(self) -> bool:
+        """Get the hang up on close setting."""
+        assert self._serial is not None
+        return self._serial.hang_up_on_close
 
     @property
     def exclusive(self) -> bool:
