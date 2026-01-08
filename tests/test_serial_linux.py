fd7c2ad11cea3c0f7ea4996cb3e93834e0c5a632
puddly <32534428+puddly@users.noreply.github.com>
2026-01-08 12:23:52 -0500

    Add a unit test

---
+++ tests/test_serial_linux.py
@@ -0,0 +1,35 @@
+"""Linux serial port tests."""
+
+import errno
+import fcntl
+from typing import Any
+from unittest.mock import ANY, call, patch
+
+from serialx.platforms.serial_posix import PosixSerial
+from tests.common import create_socat_pair
+
+TIOCSSERIAL = 0x0000541F
+TIOCGSERIAL = 0x0000541E
+
+
+@patch("serialx.platforms.serial_posix.TIOCGSERIAL", TIOCGSERIAL)
+@patch("serialx.platforms.serial_posix.TIOCSSERIAL", TIOCSSERIAL)
+def test_tiocgserial_ioctl_not_supported() -> None:
+    """Test that TIOCGSERIAL ioctl not supported is handled gracefully."""
+    ioctl_orig = fcntl.ioctl
+
+    def ioctl(fd: int, request: int, arg: Any = 0, mutate_flag: bool = True) -> None:
+        if request in (TIOCGSERIAL, TIOCSSERIAL):
+            raise OSError(errno.EOPNOTSUPP, "Not supported")
+
+        return ioctl_orig(fd, request, arg, mutate_flag)
+
+    with patch(
+        "serialx.platforms.serial_posix.fcntl.ioctl", side_effect=ioctl
+    ) as mock_ioctl:
+        with create_socat_pair() as (left, _right):
+            with PosixSerial(left, baudrate=115200):
+                # The serial port still opens
+                pass
+
+    assert call(ANY, TIOCGSERIAL, ANY) in mock_ioctl.mock_calls
