bc4d0534f8af4c658b66f50050edf04486800fde
puddly <32534428+puddly@users.noreply.github.com>
2026-02-28 19:21:44 -0500

    Connect after the `is_closing` check

--- serialx/platforms/serial_posix.py
+++ serialx/platforms/serial_posix.py
@@ -517,11 +517,12 @@ class PosixSerialTransport(DescriptorTransport, BaseSerialTransport):
         await asyncio.sleep(AFTER_OPEN_DELAY)
 
         await self._loop.run_in_executor(None, self._serial.configure_port)
-        await super()._connect()
 
         if self.is_closing():
             # If we are closing, we should not call `connection_made`
             return
+
+        await super()._connect()
 
         self._protocol.connection_made(self)
 
