a863a0446417ac9d06d18e235aee634680804d50
puddly <32534428+puddly@users.noreply.github.com>
2026-03-07 21:51:03 -0500

    Ignore mypy error with `asyncio.timeout`

--- serialx/platforms/serial_esphome.py
+++ serialx/platforms/serial_esphome.py
@@ -226,7 +226,7 @@ class ESPHomeSerial(BaseSerial):
             return 0
 
     async def _async_readinto(self, b: Buffer, timeout: float | None) -> int:
-        async with asyncio.timeout(timeout):
+        async with asyncio.timeout(timeout):  # type: ignore[attr-defined]
             while not self._read_buffer:
                 self._read_event.clear()
                 await self._read_event.wait()
