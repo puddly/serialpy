48f0d9b2254a350bc8ca39614947dd8d653f1927
puddly <32534428+puddly@users.noreply.github.com>
2026-04-09 21:47:10 -0400

    Remove unnecessary re-export of `serial_for_url`

--- serialx/__init__.py
+++ serialx/__init__.py
@@ -1,7 +1,5 @@
 """serialx serial port implementation."""
 
-from typing import Any
-
 from .async_serial import (
     SerialStreamWriter,
     create_serial_connection,
@@ -33,11 +31,15 @@ from .compat import (
 )
 from .platforms import Serial, SerialTransport, list_serial_ports
 
+# Backwards compatibility export
+serial_for_url = Serial.from_url
+
 __all__ = [
     "create_serial_connection",
     "get_serial_classes",
     "list_serial_ports",
     "open_serial_connection",
+    "serial_for_url",
     "ModemPins",
     "Parity",
     "PinState",
@@ -62,9 +64,3 @@ __all__ = [
     "CR",
     "LF",
 ]
-
-
-def serial_for_url(url: str, *args: Any, **kwargs: Any) -> BaseSerial:
-    """Create a serial port for the given URL."""
-    serial_cls, _serial_transport = get_serial_classes(url)
-    return serial_cls(url, *args, **kwargs)
