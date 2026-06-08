c3d4c26dae07006d5e807b9edcb3bb7577ddf497
puddly <32534428+puddly@users.noreply.github.com>
2026-06-08 11:53:53 -0400

    Revert

--- tests/test_async_lifecycle.py
+++ tests/test_async_lifecycle.py
@@ -17,7 +17,7 @@ import warnings
 import pytest
 
 from serialx import BaseSerialTransport, create_serial_connection
-from tests.common import SerialBackend, SerialPair, SerialQuirk
+from tests.common import SerialBackend, SerialPair
 
 
 class ProtocolState(enum.Enum):
@@ -243,7 +243,6 @@ async def test_lifecycle_close_after_abort_one_connection_lost(
 # --- Drain vs. abort semantics ---
 
 
-@pytest.mark.skip_quirks(SerialQuirk.NO_PEER_DRAIN_ON_CLOSE)
 async def test_lifecycle_close_drains_pending_writes(
     serial_pair: SerialPair,
 ) -> None:
